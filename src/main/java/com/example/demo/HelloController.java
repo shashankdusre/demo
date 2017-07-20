806d9f2598e71080569585379dbc777e4ed72383
Unknown <unknown@example.com>
2017-07-20 11:39:31 +0530

    1st commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello.htm")
+    public String index() {
+        return "index";
+    }
+    @RequestMapping("/whoami")
+    @ResponseBody
+    public Object whoami() {
+        return "rudrasish";
+    } 
+
+}
